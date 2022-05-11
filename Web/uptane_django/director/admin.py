2a1fab332cc95e73bf66cf9039e3a4c5cbc621d3
Vorrad <15217340356@163.com>
2022-05-11 16:50:11 +0800

    修改工作区为新建django项目，并添加Uptane_Demo的实现

--- Web/uptane_django/director/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Ecu,Vehicle,Target
-# Register your models here.
-admin.site.register(Ecu)
-admin.site.register(Vehicle)
-admin.site.register(Target)
--- Web/uptane_django/image/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import ImageTarget
-# Register your models here.
-admin.site.register(ImageTarget)
