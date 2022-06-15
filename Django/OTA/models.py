68c8cc8d57d51419f2706bd26b3851a55ef20893
Vorrad <89284700+Vorrad@users.noreply.github.com>
2022-06-15 17:31:42 +0800

    删除了数据库部分操作

--- Django/OTA/models.py
+++ Django/OTA/models.py
@@ -1,10 +1,3 @@
 from django.db import models
-from datetime import datetime
-class UserInfo(models.Model):
-    time = models.DateTimeField(default=datetime.now)
-    name = models.TextField(max_length=64)
-    device = models.TextField(max_length=64)
-    version = models.TextField(max_length=64)
-    reporter = models.TextField(max_length=128)
 
-
+# Create your models here.
